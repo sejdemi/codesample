c1e9efa60a6497d62233068a0efda3f521fcea37
sejdemi <sejan.miah@gmail.com>
2018-04-12 17:33:28 -0400

    Create typealias JSON for readability

--- CodeSample/CodeSample/OkCupidAPIClient.swift
+++ CodeSample/CodeSample/OkCupidAPIClient.swift
@@ -8,19 +8,28 @@
 import Foundation
 import Alamofire
 
+//violation of single responsibility principles,
+
+//define typeAlias
+typealias JSON = [String : Any]
+
 final class OkCupidAPIClient {
   
   //created as a final class because no one else should be subclassing this
-  static func getProfileMatches(for completion: @escaping (OkCupidAPIClientResponse, [MatchedUserProfile]) -> ())  {
+  static func getProfileMatches(for completion: @escaping (OkCupidAPIClientResponse, [MatchedUserProfile?]) -> ())  {
     
     var matchedUserProfiles: [MatchedUserProfile] = []
     
     let url = "https://www.okcupid.com/matchSample.json"
     
     Alamofire.request(url, method: .get).responseJSON { (response) in
-      guard let json = response.result.value as? [String: Any] else {return}
       
-      if let responseJSON = json["data"] as? [[String: Any]] {
+      //need to return something better/is guard the way to go?
+      //saw this guard let rawData = data else { handler(false, nil); return }
+
+      guard let json = response.result.value as? JSON else {return}
+      
+      if let responseJSON = json["data"] as? [JSON] {
         
         for matchedUserProfile in responseJSON {
           if let userProfile = MatchedUserProfile(dictionary: matchedUserProfile){
@@ -29,7 +38,6 @@ final class OkCupidAPIClient {
             print("The count is \(matchedUserProfiles.count)")
           }
         }
-        
         completion(.success(), matchedUserProfiles)
         
       } else {
