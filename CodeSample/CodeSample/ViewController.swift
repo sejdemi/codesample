32a2f15f0a51508c2f72a0710a24e9791c1ba2ba
sejdemi <sejan.miah@gmail.com>
2018-04-11 21:52:25 -0400

    Create preliminary collectionView UI

---
+++ CodeSample/CodeSample/ViewController.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
+
+//// MARK: - UITableViewDataSource
+//extension MyViewController: UITableViewDataSource {
+//    // table view data source methods
+//}
+//
+//// MARK: - UIScrollViewDelegate
+//extension MyViewController: UIScrollViewDelegate {
+//    // scroll view delegate methods
+//}
