acd2933f90797dc55d1892b1c9ab998bf248775e
sejdemi <sejan.miah@gmail.com>
2018-04-16 21:25:51 -0400

    Refactor collectionView layout

--- CodeSample/CodeSample/Controller/MatchedProfilesViewController.swift
+++ CodeSample/CodeSample/Controller/MatchedProfilesViewController.swift
@@ -4,10 +4,16 @@ import Kingfisher
 class MatchedProfilesViewController: UIViewController {
   @IBOutlet weak var profileCollectionView: UICollectionView!
   var profileMatches: [MatchedUserProfile] = []
+  public var screenWidth: CGFloat {
+    return UIScreen.main.bounds.width
+  }
+  public var screenHeight: CGFloat {
+    return UIScreen.main.bounds.height
+  }
   
   override func viewDidLoad() {
     super.viewDidLoad()
-
+    
     OkCupidAPIClient.fetchAllMatchedUsers(completion: { [weak self] (clientResponse, matchedUserProfiles) in
       guard let matchedUserProfiles = matchedUserProfiles else {
         return
@@ -35,5 +41,45 @@ extension MatchedProfilesViewController: UICollectionViewDataSource {
     cell.animate()
     return cell
   }
+  
+}
+
+extension MatchedProfilesViewController: UICollectionViewDelegateFlowLayout {
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      sizeForItemAt indexPath: IndexPath) -> CGSize {
+    var cellWidth: CGFloat
+    var cellHeight: CGFloat
+    if screenWidth < 375 {
+       cellWidth = screenWidth * 0.425
+       cellHeight = screenHeight * 0.414
+    } else {
+       cellWidth = screenWidth * 0.44
+       cellHeight = screenHeight * 0.429
+    }
+    let cellSize = CGSize(width: cellWidth, height: cellHeight)
+    return cellSize
+  }
+  
+  func collectionView(_ collectionView: UICollectionView,
+                      layout collectionViewLayout: UICollectionViewLayout,
+                      insetForSectionAt section: Int) -> UIEdgeInsets {
+    return UIEdgeInsets(top: 15, left: 15, bottom: 15, right: 15)
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return 15
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    return 15
+  }
+  
+  
+  
+  
   
 }
--- CodeSample/CodeSample/Networking/OkCupidAPIClient.swift
+++ CodeSample/CodeSample/Networking/OkCupidAPIClient.swift
@@ -5,7 +5,7 @@ typealias JSON = [String: Any]
 
 final class OkCupidAPIClient {
   
-  static func fetchAllMatchedUsers(completion: @escaping (OkCupidAPIClientResponse, [MatchedUserProfile]?)-> ()) {
+  static func fetchAllMatchedUsers(completion: @escaping (OkCupidAPIClientResponse, [MatchedUserProfile]?) -> ()) {
     let url = "https://www.okcupid.com/matchSample.json"
     Alamofire.request(url, method: .get).responseJSON { (response) in
       if let json = response.result.value as? JSON  {
@@ -17,7 +17,7 @@ final class OkCupidAPIClient {
     }
   }
   
-  static func parseUserJSON(with json: JSON)-> [MatchedUserProfile] {
+  static func parseUserJSON(with json: JSON) -> [MatchedUserProfile] {
     var matchedUserProfiles: [MatchedUserProfile] = []
     if let responseJSON = json["data"] as? [JSON] {
       for matchedUserProfile in responseJSON {
